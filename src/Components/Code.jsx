ee541fdc88e0138ca4762bdb407e92417000ee22
Anukulsingh01 <anukulsinghbisht@gmail.com>
2025-07-30 16:01:38 +0530

    Update Code.jsx and config files for responsive layout

--- src/Components/Code.jsx
+++ src/Components/Code.jsx
@@ -6,28 +6,56 @@ import { DataContext } from '../contexts/DataProvider';
 
 const Container=styled(Box)`
 display:flex;
+flex-wrap:wrap;
 background-color:#060606;
+ `;
+// const Container = styled(Box)(({ theme }) => ({
+//   display: 'flex',
+//   backgroundColor: '#060606',
+
+//   // Custom media query for width < 800px
+//   [`@media (max-width:800px)`]: {
+//     flexDirection: 'column',
+//   },
+// }));
+const CoderWrapper = styled(Box)`
+  min-width: 250px;
+  flex: 1; /* optional: allows it to grow */
 `;
+
 const Code=()=>{
     const{html,setHtml,css,setCss,js,setJs}=useContext(DataContext);
     return(
         <Container>
-        <Editor heading="HTML" 
-        sign="/"
-        bg="red" 
-        value={html}
-        onChange={setHtml}/>
-        <Editor 
-        heading="CSS"
-        sign="*"
-        bg="blue"
-         value={css} 
-        onChange={setCss}
-        />
-        <Editor heading="JS" sign="<>" bg="yellow"
-        value={js}
-        onChange={setJs}/>
-        </Container>
+  <CoderWrapper>
+    <Editor
+      heading="HTML"
+      sign="/"
+      bg="red"
+      value={html}
+      onChange={setHtml}
+    />
+  </CoderWrapper>
+  <CoderWrapper>
+    <Editor
+      heading="CSS"
+      sign="*"
+      bg="blue"
+      value={css}
+      onChange={setCss}
+    />
+  </CoderWrapper>
+  <CoderWrapper>
+    <Editor
+      heading="JS"
+      sign="<>"
+      bg="yellow"
+      value={js}
+      onChange={setJs}
+    />
+  </CoderWrapper>
+</Container>
+
     );
 }
 export default Code;
